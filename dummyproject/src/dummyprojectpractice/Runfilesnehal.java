022385259df9abdbb9384d83cd40987e3376f0ad
Siva Kumar <Siva Kumar@DESKTOP-C7P4CQB>
2025-03-25 11:44:46 +0530

    hello

--- dummyproject/src/dummyprojectpractice/Runfilesnehal.java
+++ dummyproject/src/dummyprojectpractice/Runfilesnehal.java
@@ -5,7 +5,7 @@ public class Runfilesnehal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("hai snehal");
+		System.out.println("hai snehal sneha");
 
 	}
 
