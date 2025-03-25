b8547728227d4f88dab94b21e7e398162566f28f
Siva Kumar <Siva Kumar@DESKTOP-C7P4CQB>
2025-03-25 11:39:54 +0530

    hai

---
+++ dummyproject/src/dummyprojectpractice/Runfilesnehal.java
@@ -0,0 +1,12 @@
+package dummyprojectpractice;
+
+public class Runfilesnehal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hai snehal");
+
+	}
+
+}
